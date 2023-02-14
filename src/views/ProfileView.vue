d5a3190ba2c54ba9abe6473b142150bc2fbddf78
Numan Iftikhar <numaniftikhar300@gmail.com>
2023-02-14 22:33:30 +0500

    design profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,12 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div
+    class="container mx-auto flex justify-center items-center min-h-[100vh] flex-col"
+  >
+    <div class="camera border-2 h-96 w-2/3">camera</div>
+    <div
+      class="mt-5 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Capture
+    </div>
+  </div>
 </template>
